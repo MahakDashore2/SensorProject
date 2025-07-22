66a443e2d97fdeea04cf576a51fae9a33706619c
unknown <dashoremahak@gmail.com>
2025-07-22 11:41:25 +0530

    modules added

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+# imp to keep track events that happens when application runs which help in debugging and monitoring the application behaviors
+
+import logging
+import os # to make files and directories and write inside them
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+
